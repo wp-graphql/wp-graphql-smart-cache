91aefa769a1b3f94fea947e05b21f57e43b9a894
Mark Kelnar <749603+markkelnar@users.noreply.github.com>
2021-10-18 11:29:41 -0500

    admin test adjustment

--- tests/acceptance/SaveQueryGrantCest.php
+++ tests/acceptance/SaveQueryGrantCest.php
@@ -16,17 +16,16 @@ class SaveQueryGrantCest
 
         // Now select different option and verify the status
         $I->selectOption('graphql_query_grant', 'allow');
-        $I->click('Publish');
-        $I->seeOptionIsSelected('form input[name=graphql_query_grant]', 'allow');
+        $I->click('publish');
+        $I->seeOptionIsSelected('form input[id=graphql_query_grant_allow]', 'allow');
 
         $I->selectOption('graphql_query_grant', 'deny');
-        $I->click('Publish');
-        $I->seeOptionIsSelected('form input[name=graphql_query_grant]', 'deny');
+        $I->click('publish');
+        $I->seeOptionIsSelected('form input[id=graphql_query_grant_deny]', 'deny');
 
         // Check the listing on the admin page table list of queries
         $I->amOnPage('/wp-admin/edit.php?post_type=graphql_query');
-        // td[3] is the 'Allow/Deny' column name
-        $I->see('deny', "//table/tbody/tr[1]/td[3]");
+        $I->see('deny', '//table/tbody/tr[1]/td[@data-colname="Allow/Deny"]/a');
     }
  
 }
