9192886d91d743306f479f80bb4a63efd5338ad6
Mark Kelnar <749603+markkelnar@users.noreply.github.com>
2021-10-19 09:20:33 -0500

    Use filter to translate Excerpt text string

--- src/SavedQueryDescription.php
+++ src/SavedQueryDescription.php
@@ -10,12 +10,28 @@ namespace WPGraphQL\PersistedQueries;
 class SavedQueryDescription {
 
 	public function init() {
-		add_post_type_support( 'graphql_query', 'excerpt' );
+		// Enable excerpts for the persisted query post type
+		add_post_type_support( SavedQuery::TYPE_NAME, 'excerpt' );
 
-		add_filter( 'manage_graphql_query_posts_columns', [ $this, 'filter_add_description_to_admin' ], 10, 1);
-		add_action( 'manage_graphql_query_posts_custom_column', [ $this, 'action_fill_excerpt_content' ], 10, 2);
-		add_filter( 'manage_edit-graphql_query_sortable_columns', [ $this, 'filter_make_excerpt_column_sortable_in_admin' ], 10, 1 );
-		add_action( 'add_meta_boxes_graphql_query', [ $this, 'action_the_excerpt_admin_meta_box' ], 10, 1);
+		// Change the text from Excerpt to Description where it is visible.
+		add_filter( 'gettext', [ $this, 'filter_translate_excerpt_text' ], 10, 1 );
+
+		add_filter( sprintf( 'manage_%s_posts_columns', SavedQuery::TYPE_NAME ), [ $this, 'filter_add_description_to_admin' ], 10, 1 );
+		add_action( sprintf( 'manage_%s_posts_custom_column', SavedQuery::TYPE_NAME ), [ $this, 'action_fill_excerpt_content' ], 10, 2 );
+		add_filter( sprintf( 'manage_edit-%s_sortable_columns', SavedQuery::TYPE_NAME ), [ $this, 'filter_make_excerpt_column_sortable_in_admin' ], 10, 1 );
+	}
+
+	/**
+	 * Change the text from Excerpt to Description where it is visible.
+	 *
+	 * @param String  The string for the __() or _e() translation
+	 * @return String  The translated or original string
+	 */
+	public function filter_translate_excerpt_text( $string ) {
+		if ( 'Excerpt' === $string ) {
+			return __( 'Description', 'wp-graphql-persisted-queries' );
+		}
+		return $string;
 	}
 
 	/**
@@ -29,25 +45,13 @@ class SavedQueryDescription {
 
 	public function action_fill_excerpt_content( $column, $post_id ) {
 		if ( 'excerpt' === $column ) {
-			echo get_the_excerpt( $post_id );
+			echo esc_html( get_the_excerpt( $post_id ) );
 		}
-	  }
+	}
 
 	public function filter_make_excerpt_column_sortable_in_admin( $columns ) {
 		$columns['excerpt'] = true;
 		return $columns;
 	}
 
-	public function action_the_post_excerpt_admin_meta_box( $post ) {
-		global $wp_meta_boxes;
-	
-		$page = 'graphql_query';
-		$context = 'normal';
-		$priority = 'core';
-		$id = 'postexcerpt';
-
-		if ( isset( $wp_meta_boxes[ $page ][ $context ][ $priority ][ $id ] ) ) {
-			$wp_meta_boxes[ $page ][ $context ][ $priority ][ $id ]['title'] = __('Description', 'wp-graphql-persisted-queries');
-		}
-	}
 }
