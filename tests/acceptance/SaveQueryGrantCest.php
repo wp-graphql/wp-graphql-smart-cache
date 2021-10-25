05a33716edc0d381769c7b9d311077a364f40a56
Mark Kelnar <749603+markkelnar@users.noreply.github.com>
2021-10-25 10:32:49 -0500

    Use form submit name save

--- tests/acceptance/SaveQueryGrantCest.php
+++ tests/acceptance/SaveQueryGrantCest.php
@@ -20,12 +20,12 @@ class SaveQueryGrantCest
         $I->seeOptionIsSelected('form input[id=graphql_query_grant_allow]', 'allow');
 
         $I->selectOption('graphql_query_grant', 'deny');
-        $I->click('publish');
+        $I->click('save');
         $I->seeOptionIsSelected('form input[id=graphql_query_grant_deny]', 'deny');
 
         // Check the listing on the admin page table list of queries
         $I->amOnPage('/wp-admin/edit.php?post_type=graphql_query');
         $I->see('deny', '//table/tbody/tr[1]/td[@data-colname="Allow/Deny"]');
     }
- 
+
 }
