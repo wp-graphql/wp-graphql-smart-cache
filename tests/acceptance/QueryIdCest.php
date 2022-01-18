9b501b01a73004382318ebde95232b6a8b0d83f7
Mark Kelnar <749603+markkelnar@users.noreply.github.com>
2022-01-18 10:37:55 -0600

    Add timer to use for future cache tests

--- tests/acceptance/QueryIdCest.php
+++ tests/acceptance/QueryIdCest.php
@@ -2,10 +2,6 @@
 
 class QueryIdCest
 {
-    public function _before(AcceptanceTester $I)
-    {
-    }
-
     public function queryIdThatDoesNotExistTest(AcceptanceTester $I)
     {
         $I->sendGet('graphql', [ 'queryId' => '1234' ] );
