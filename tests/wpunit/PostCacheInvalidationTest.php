45defe203cc881b73cf41eca6666057079de24cb
Jason Bahl <jasonbahl@mac.com>
2022-05-31 10:17:53 -0600

    - update composer.json to have autoloading for tests - add tests/_data/config.php as an autoload file for tests - add tests/_data/config.php file to define constants in test environment (GRAPHQL_DEBUG) - Add WPGraphQLLabsTestCase.php with some re-usable functions, queries, etc - Update PostCacheInvalidationTest.php to extend WPGraphQLLAbsTestCase - update description in SettingsCacheInvalidationTest.php to better articulate some thoughts

--- tests/wpunit/PostCacheInvalidationTest.php
+++ tests/wpunit/PostCacheInvalidationTest.php
@@ -1,31 +1,12 @@
 <?php
-namespace WPGraphQL\Labs;
 
-use WPGraphQL\Labs\Cache\Collection;
-
-class PostCacheInvalidationTest extends \Codeception\TestCase\WPTestCase {
-
-	protected $collection;
+class PostCacheInvalidationTest extends \TestCase\WPGraphQLLabs\TestCase\WPGraphQLLabsTestCase {
 
 	public function setUp(): void {
-		\WPGraphQL::clear_schema();
-
-		if ( ! defined( 'GRAPHQL_DEBUG' ) ) {
-			define( 'GRAPHQL_DEBUG', true );
-		}
-
-		$this->collection = new Collection();
-
-		// enable caching for the whole test suite
-		add_option( 'graphql_cache_section', [ 'cache_toggle' => 'on' ] );
-
 		parent::setUp();
 	}
 
 	public function tearDown(): void {
-		\WPGraphQL::clear_schema();
-		// disable caching
-		delete_option( 'graphql_cache_section' );
 		parent::tearDown();
 	}
 
@@ -33,7 +14,161 @@ class PostCacheInvalidationTest extends \Codeception\TestCase\WPTestCase {
 		$this->assertTrue( true );
 	}
 
-	// scheduled post is published
+//	public function postListQuery() {
+//		return '
+//		{
+//		  posts {
+//		    nodes {
+//		      __typename
+//		      databaseId
+//		    }
+//		  }
+//		}
+//		';
+//	}
+//
+//	public function contentNodesQuery() {
+//		return '
+//		{
+//		  contentNodes {
+//		    nodes {
+//		      __typename
+//		      databaseId
+//		    }
+//		  }
+//		}
+//		';
+//	}
+//
+//	public function singlePostByDatabaseIdQuery() {
+//
+//		return '
+//			query GetPost($id:ID!) {
+//			  post(id:$id idType:DATABASE_ID) {
+//				__typename
+//				databaseId
+//			  }
+//			}
+//		';
+//	}
+//
+//	// execute several queries to populate the cache and assert that the cache is populated
+//	public function populateCache() {
+//
+//		$queries = [
+//			'single_post' => [
+//				'query' => $this->singlePostByDatabaseIdQuery(),
+//				'variables' => [ $this->post_id ]
+//			],
+//			'post_list' => [
+//				'query' => $this->postListQuery(),
+//			],
+//			'content_nodes' => [
+//				'query' => $this->contentNodesQuery(),
+//			],
+//			'page' => [
+//				'query' => null
+//			]
+//		];
+//
+//	}
+//
+//	/**
+//	 * @throws Exception
+//	 */
+//	public function executeQueryAndGetResults( $query, $variables ) {
+//
+//		$results = graphql([
+//			'query' => $query,
+//			'variables' => $variables,
+//		]);
+//
+//		return [
+//			'results' => $results,
+//			'cacheKey' => $this->collection->build_key( null, $query, $variables ),
+//			'query' => $query,
+//			'variables' => $variables,
+//		];
+//	}
+//
+//	/**
+//	 * Test behavior when a scheduled post is published
+//	 *
+//	 * - given:
+//	 *   - a query for a single pre-existing post is in the cache
+//	 *   - a query for a list of posts is in the cache
+//	 *   - a query for contentNodes is in the cache
+//	 *   - a query for a page is in the cache
+//	 *   - a query for a list of pages is in the cache
+//	 *   - a query for a tag is in the cache
+//	 *   - a query for a list of tags is in the cache
+//	 *   - a query for a list of users is in the cache
+//	 *   - a query for the author of the post is in the cache
+//	 *
+//	 * - when:
+//	 *   - a scheduled post is published
+//	 *
+//	 * - assert:
+//	 *   - query for list of posts is invalidated
+//	 *   - query for contentNodes is invalidated
+//	 *   - query for single pre-exising post remains cached
+//	 *   - query for a page remains cached
+//	 *   - query for list of pages remains cached
+//	 *   - query for tag remains cached
+//	 *   - query for list of tags remains cached
+//	 *   - query for list of users remains cached
+//	 *   - query for the author of the post remains cached
+//	 *
+//	 *
+//	 * @throws Exception
+//	 */
+//	public function testScheduledTestIsPublished() {
+//
+//		// post caches should start empty
+//		$post_cache = $this->collection->get( 'post' );
+//		$post_list_cache = $this->collection->get( 'list:post' );
+//		$this->assertEmpty( $post_cache );
+//		$this->assertEmpty( $post_list_cache );
+//
+//		$scheduled_post_id = self::factory()->post->create([
+//			'post_type' => 'post',
+//			'post_status' => 'future',
+//			'post_title' => 'Test Scheduled Post',
+//			'post_author' => $this->admin,
+//			'post_date' => date( "Y-m-d H:i:s", strtotime( '+1 day' ) )
+//		]);
+//
+//		// after a future post is created, these caches should still be empty
+//		$post_cache = $this->collection->get( 'post' );
+//		$post_list_cache = $this->collection->get( 'list:post' );
+//		$this->assertEmpty( $post_cache );
+//		$this->assertEmpty( $post_list_cache );
+//
+//		$results = $this->executeQueryAndGetResults(
+//			$this->singlePostByDatabaseIdQuery(),
+//			[
+//				'id' => $scheduled_post_id,
+//			]
+//		);
+//
+//		// after executing, the post cache should have data
+//		$post_cache = $this->collection->get( 'post' );
+//		$post_list_cache = $this->collection->get( 'list:post' );
+//		$this->assertNotEmpty( $post_cache );
+//		$this->assertEmpty( $post_list_cache );
+//
+//		$this->assertEquals(
+//			$this->collection->get( $results['cacheKey'] ),
+//			$results['results']
+//		);
+//
+//		// publish the post
+//		wp_publish_post( $scheduled_post_id );
+//
+//
+//
+//	}
+
 	// published post is changed to draft
 	// published post is changed to private
 	// published post is trashed
--- tests/wpunit/SettingsCacheInvalidationTest.php
+++ tests/wpunit/SettingsCacheInvalidationTest.php
@@ -33,6 +33,38 @@ class SettingsCacheInvalidationTest extends \Codeception\TestCase\WPTestCase {
 		$this->assertTrue( true );
 	}
 
+	// @todo I think many updates to settings might require a purge_all.
+	// for example, if I change something like the timezone or the site language,
+	// that will likely have wide impact on how resolvers execute and should probably
+	// trigger wide reaching purge event.
+	// I think there's probably going to need to be some
+	// case-by-case decisions made here for how settings should
+	// actually impact cache.
+	//
+	// like the "general" settings group seems pretty far reaching
+	// but the "writing" settings doesn't seem like
+	// it would have _as much_ of an impact.
+	// if I were to change some setting like the "default post category" in the writing settings,
+	// that wouldn't necessitate a purge_all()
+	//
+	// the reading settings probably have wider reach though too.
+	// if I change the homepage display option, that will impact several things.
+	//
+	// I'm thinking we need some way to either purge queries for a settings group,
+	// purge_all or do nothing.
+	//
+	//
+	// for example,
+	// a change to general_settings group should probably purge_all
+	// a change to writing settings should probably only purge queries like { writingSettings { ... } }
+
+
+
+
+
+
+
+
 	// update untracked options (no purge)
 	// update tracked option (purge group?)
 	// set transient doesn't purge (don't want weird infinite loops)
@@ -45,5 +77,8 @@ class SettingsCacheInvalidationTest extends \Codeception\TestCase\WPTestCase {
 	// taxonomy removed?
 	// schema breaking change detected?
 	// update permalinks (purge all?)
+
+
+
 
 }
