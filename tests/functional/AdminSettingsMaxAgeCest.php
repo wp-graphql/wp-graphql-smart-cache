43e9fca0def1da77e73463e00c5b55fe06de6fa0
Mark Kelnar <749603+markkelnar@users.noreply.github.com>
2022-06-28 15:07:42 -0500

    Fix global_ttl setting section and tests

--- tests/functional/AdminSettingsMaxAgeCest.php
+++ tests/functional/AdminSettingsMaxAgeCest.php
@@ -7,19 +7,19 @@
 class AdminSettingsMaxAgeCest
 {
 	public function _after( FunctionalTester $I ) {
-		$I->dontHaveOptionInDatabase( 'graphql_persisted_queries_section'  );
+		$I->dontHaveOptionInDatabase( 'graphql_cache_section'  );
 	}
 
 	public function saveMaxAgeSettingsTest( FunctionalTester $I ) {
 			$I->loginAsAdmin();
 
-			$I->amOnPage('/wp-admin/admin.php?page=graphql-settings#graphql_persisted_queries_section');
-			$I->seeInField(['name' => 'graphql_persisted_queries_section[global_max_age]'], null);
-			$I->fillField(['name' => 'graphql_persisted_queries_section[global_max_age]'], '30');
+			$I->amOnPage('/wp-admin/admin.php?page=graphql-settings#graphql_cache_section');
+			$I->seeInField(['name' => 'graphql_cache_section[global_max_age]'], null);
+			$I->fillField(['name' => 'graphql_cache_section[global_max_age]'], '30');
 
 			// Save and see the selection after form submit
 			$I->click('Save Changes');
-			$I->seeInField(['name' => 'graphql_persisted_queries_section[global_max_age]'], '30');
+			$I->seeInField(['name' => 'graphql_cache_section[global_max_age]'], '30');
 	}
 
 }
