5b888e8ab6d3d89372a98d07e5fd284975e9dfa7
Carmine Colicino <carminecolicino@gmail.com>
2023-04-24 15:43:23 +0200

    Fix post_exists() PHP fatal error

--- src/Cache/Invalidation.php
+++ src/Cache/Invalidation.php
@@ -558,11 +558,11 @@ class Invalidation {
 		// get the post object being modified
 		$post = get_post( $post_id );
 
-		// Check if $post_id is valid: Make sure that $post_id is a valid post ID. 
-		if ( ! post_exists( $post_id ) ) {
+		// Make sure that $post is a valid post object.
+		if ( empty( $post ) ) {
 			return;
 		}
-		
+
 		// if the post type is not tracked, ignore it
 		if ( ! in_array( $post->post_type, \WPGraphQL::get_allowed_post_types(), true ) ) {
 			return;
