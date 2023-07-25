a24925716fc40ee62117d50345e3631bf4f786b0
Jason Bahl <jasonbahl@mac.com>
2023-07-25 09:56:24 -0600

    - set uniqid to false

--- tests/wpunit/BatchQueryTest.php
+++ tests/wpunit/BatchQueryTest.php
@@ -21,7 +21,7 @@ class BatchQueryTest extends \Tests\WPGraphQL\TestCase\WPGraphQLTestCase {
 
 		// Create/save persisted query for the query and query id
 		// The uniqid manes it's different between test runs, in case something fails and is stuck in database.
-		$this->query_alias = uniqid( "query_posts_", true );
+		$this->query_alias = uniqid( "query_posts_", false );
 		$query_string = sprintf( "query %s { posts { nodes { id title } } }", $this->query_alias );
 
 		$saved_query = new Document();
@@ -91,7 +91,7 @@ class BatchQueryTest extends \Tests\WPGraphQL\TestCase\WPGraphQLTestCase {
 
 		// Create/save persisted query for the query and query id
 		// Set the max age for a saved query
-		$query_alias_1 = uniqid( "query_posts_", true );
+		$query_alias_1 = uniqid( "query_posts_", false );
 		$query_string = sprintf( "query %s { posts { nodes { id title } } }", $query_alias_1 );
 		$query_post_id = $saved_query->save( $query_alias_1, $query_string );
 		$max_age = new MaxAge();
@@ -100,7 +100,7 @@ class BatchQueryTest extends \Tests\WPGraphQL\TestCase\WPGraphQLTestCase {
 
 		// Create/save persisted query for the query and query id
 		// Set the max age for a saved query
-		$query_alias_2 = uniqid( "query_posts_", true );
+		$query_alias_2 = uniqid( "query_posts_", false );
 		$query_string_2 = sprintf( "query %s { posts { nodes { id title } } }", $query_alias_2 );
 		$query_post_id = $saved_query->save( $query_alias_2, $query_string_2 );
 		$max_age = new MaxAge();
